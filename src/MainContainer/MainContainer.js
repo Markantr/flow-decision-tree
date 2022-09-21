3340610be211f5e179c545a5fe3e95152900fcb3
Robin Markant <markantr@bzz.ch>
2022-09-21 13:54:49 +0200

    Starting Screen

--- src/MainContainer/MainContainer.js
+++ src/MainContainer/MainContainer.js
@@ -1,12 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import NameSection from "./NameSection";
 import DecisionTree from "../DecisionTree/DecisionTree";
-import DecisionTree2 from "../DecisionTree/DecisionTree2";
 
-class MainContainer extends Component {
-  state = {};
+export default class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ showDecisionTree: true });
+  }
+
   render() {
-    return <DecisionTree2 />;
+    const { showDecisionTree } = this.state;
+    const { handleClick } = this.state;
+    return (
+      <div className="mainContainer">
+        <h2>You want to ...</h2>
+        {!showDecisionTree && <NameSection handleClick={this.handleClick} />}
+        {showDecisionTree && <DecisionTree />}
+      </div>
+    );
   }
 }
-
-export default MainContainer;
